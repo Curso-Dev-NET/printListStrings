e6d8e57e06d5773285a01ff5c60605ab1744a424
xandelm <alexandrej.lima.mendes@gmail.com>
2023-07-31 07:41:37 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<string> nomes = new();
+nomes.AddRange(new []{".net","2023","ifnmg"});
+// Console.WriteLine(nomes); imprime o tipo
+Console.WriteLine(String.Join("\n",nomes));
+
